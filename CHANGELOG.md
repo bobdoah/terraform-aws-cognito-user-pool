52fe881d481820947fa1d1456269dd0e96c9fd95
Robert Williams <robert.williams@nominet.uk>
2021-08-06 16:58:33 +0100

    Revert "Update minimum required Terraform and AWS provider version"
    This reverts commit c6798f9e0f2521c8cb75f380384e152f059f398a.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,3 @@
-## 0.12.0 (July 4, 2021)
-
-FIXES:
-
-* set client_secrets output to be sensitive (thanks @sapei)
-
 ## 0.11.1 (May 21, 2021)
 
 FIXES:
--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 0.12.9"
 
   required_providers {
-    aws = "~> 3.2"
+    aws = ">= 2.54.0"
   }
 }
